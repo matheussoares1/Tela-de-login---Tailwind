f91abd00e667bde0f3c5ca3d7d2561fc08bb05a1
Matheus Soares <111543203+matheussoares1@users.noreply.github.com>
2023-03-21 08:47:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-//escreva um codigo que ao digitar a senha correta, a imagem de background estilizada no css vá diminuindo o valor de blur
+const password = document.getElementById('password');
+const background = document.getElementById('background');
 
+
+password.addEventListener('input', (e)=>{
+    const input = e.target.value;
+    /* console.log(input); */
+
+    const length = input.length;
+    /* console.log(length);  */
+
+    const blurValue = 20 - length * 2;
+    /* console.log(blurValue); */
+    background.style.filter = `blur(${blurValue}px)`;
+})
